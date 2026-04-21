02567fccf8124e08d18e00aba26d0f76b79dcc22
Emmanuel BOUGEROLLE <ebougerolle@efalia.com>
2026-04-21 08:01:18 +0200

    Revert "Initial commit"
    This reverts commit 607ff70d23074bc08829907bbca712ef641e6736.

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -1,15 +1,26 @@
 <?php
-
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-final class SecurityController extends AbstractController
+class SecurityController extends AbstractController
 {
-    #[Route('/logout', name: 'app_logout')]
-    public function logout(): never
+    #[Route('/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authUtils): Response
     {
-        throw new \LogicException('Cette méthode est interceptée par le firewall de Symfony.');
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_carte_index');
+        }
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $authUtils->getLastUsername(),
+            'error' => $authUtils->getLastAuthenticationError(),
+        ]);
     }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(): void {}
 }
